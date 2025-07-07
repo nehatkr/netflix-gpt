7a534d31a525412b0864aba7e16029d8eb67825e
Neha Kumari <110078405+nehatkr@users.noreply.github.com>
2025-07-07 07:57:19 +0530

    Fix framer-motion dependency and React Hook warnings

--- src/components/Header.js
+++ src/components/Header.js
@@ -45,7 +45,7 @@ const Header = () => {
       }
     });
     return () => unsubscribe();
-  }, []);
+  }, [dispatch, navigate]);
 
   const handleGptSearchClick = () => {
     dispatch(toggleGptSearchView());
