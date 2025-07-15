67d6aa8641b28b6d683c7dfcb7cf0cd1e3b62d08
Neha Kumari <110078405+nehatkr@users.noreply.github.com>
2025-07-15 09:29:19 +0530

    Updated Body.js

---
+++ src/components/Body.js
@@ -0,0 +1,50 @@
+import Login from "./Login";
+import Browse from "./Browse";
+import MovieDetails from "./MovieDetails";
+import WelcomePage from "./WelcomePage";
+import WatchLater from "./WatchLater";
+import UserProfile from "./UserProfile";
+import MovieRecommendations from "./MovieRecommendations";
+import { createBrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+
+const Body = () => {
+  const appRoute = createBrowserRouter([
+    {
+      path: "/",
+      element: <WelcomePage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+    {
+      path: "/movie/:movieId",
+      element: <MovieDetails />,
+    },
+    {
+      path: "/profile",
+      element: <UserProfile />,
+    },
+    {
+      path: "/watchlater",
+      element: <WatchLater />,
+    },
+    {
+      path: "/recommendations",
+      element: <MovieRecommendations />,
+    },
+  ]);
+
+  return (
+    <div>
+      <RouterProvider router={appRoute} />
+    </div>
+  );
+};
+
+export default Body;
